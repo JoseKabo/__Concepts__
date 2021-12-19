b75cd85c834720b353e65f810182b4e2b62236cc
JoseKabo <kabo.0699@gmail.com>
2021-12-19 15:50:35 -0600

    bit changes

--- Challenge1/src/Routes/users.js
+++ Challenge1/src/Routes/users.js
@@ -4,5 +4,8 @@ import { usersGET } from '../Controllers/users';
 const userRouter = Router();
 
 userRouter.get('/', usersGET);
+// userRouter.get('/:id', usersGetById);
+// userRouter.post('/new', newUserController);
+
 
 export default userRouter;
